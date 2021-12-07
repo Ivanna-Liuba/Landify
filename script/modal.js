14f9fde1be147b20eb2d088f86fccbe14ef7e6b1
Ivanna <88909372+Ivanna-Liuba@users.noreply.github.com>
2021-12-07 15:44:23 +0200

    Add files via upload

---
+++ script/modal.js
@@ -0,0 +1,12 @@
+$('#start-btn').on('click', function(){
+    $('.modal-overlay').fadeIn()
+})
+
+$('.modal-overlay').on('click', function(e){
+    if($(e.target).closest('.modal').length ==0) {
+        $(this).fadeOut()
+    }
+})
+$('.modal-close').on('click', function(){
+    $(this).parents('.modal-overlay').fadeOut()
+})
